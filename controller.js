6aa19386f5e3200cc11b39776d2ebc82af4be160
marcel <marcell032017@gmail.com>
2021-03-09 09:33:44 +0700

    menampilkan data

--- controller.js
+++ controller.js
@@ -5,4 +5,15 @@ var connection = require('./koneksi');
 
 exports.index = function(req,res){
     response.ok("aplikasi REST API berjalan", res)
+};
+
+//menampilkan semua data
+exports.tampilmahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
 }
