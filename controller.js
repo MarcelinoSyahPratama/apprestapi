876c7c4dbf3e1df05c33a9f7e663130fb0a5c1cc
marcel <marcell032017@gmail.com>
2021-03-09 09:52:29 +0700

    menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -8,6 +8,19 @@ exports.index = function(req,res){
 };
 
 //menampilkan semua data
+exports.tampilmahasiswaberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',[id],
+    function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+}
+
+//menampilkan data berdasarkan id
 exports.tampilmahasiswa = function(req,res){
     connection.query('SELECT * FROM mahasiswa',function(error, rows, fileds){
         if(error){
